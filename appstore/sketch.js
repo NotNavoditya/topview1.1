0244886b95c7ae7de154938f94949ccbd9cba30a
NotNavoditya <72127528+NotNavoditya@users.noreply.github.com>
2021-04-20 20:21:03 +0530

    Add files via upload

---
+++ appstore/sketch.js
@@ -0,0 +1,35 @@
+//var obj, objIMG;
+
+
+function draw() {
+  background(0,0,0);    
+
+//if(keyDown(UP_ARROW)){
+//plr.y = plr.y-5;
+//}
+
+  drawSprites();
+}
+
+//var obj, objIMG;
+
+function preload(){
+  //objIMG = loadImage("obj.png");
+  }
+  
+  function setup() {
+    createCanvas(1800,1400);
+    //obj = createSprite(700, 600, 50, 50);
+    //obj.addImage(objIMG);
+    //obj.scale = 1.5;
+  }
+  
+  function draw() {
+    background(0,0,0);    
+  
+//if(keyDown(UP_ARROW)){
+//plr.y = plr.y-5;
+//}
+
+    drawSprites();
+  }
